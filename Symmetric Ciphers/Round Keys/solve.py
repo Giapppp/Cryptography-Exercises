12b3c28ed3882f2247a950fe17b1a21e66711e7b
G14p <116430098+Giapppp@users.noreply.github.com>
2023-04-20 23:02:34 +0700

    Create solve.py

---
+++ Symmetric Ciphers/Round Keys/solve.py
@@ -0,0 +1,19 @@
+state = [
+    [206, 243, 61, 34],
+    [171, 11, 93, 31],
+    [16, 200, 91, 108],
+    [150, 3, 194, 51],
+]
+
+round_key = [
+    [173, 129, 68, 82],
+    [223, 100, 38, 109],
+    [32, 189, 53, 8],
+    [253, 48, 187, 78],
+]
+
+
+def add_round_key(s, k):
+    return "".join([chr(s3^k3) for s2, k2 in zip(s, k) for s3, k3 in zip(s2, k2)])
+
+print(add_round_key(state, round_key))
